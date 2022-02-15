dcdeece8730146f86f3aaa7e777ad37e3461af03
Adammmmmmmmmmmmmmmmmmmmmmmmmm <3103120005@student.smktelkom-pwt.sch.id>
2022-02-15 08:20:56 +0700

    "Modul 3"

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,5 +32,10 @@ Route::get('/about', function () {
 Route::get('/gallery', function () {
     return view('gallery', [
         "title" => "Gallery"
+    ]);
+});
+Route::get('/contacts', function () {
+    return view('contacts', [
+        "title" => "Contacts"
     ]);
 });
